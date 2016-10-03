d7a412063804fd2273d5dfd9f0a4b2ed8e873d6f
Warren MacEvoy <wmacevoy@gmail.com>
2016-10-03 05:58:40 -0600

    untested windows

--- src/path_to_self.c
+++ src/path_to_self.c
@@ -42,3 +42,37 @@ char *path_to_self()
 
 #endif
 
+#ifdef _WIN32
+
+#include <Windows.h>
+
+char *path_to_self() {
+  DWORD tcapacity = 0;
+  TCHAR *tchars = 0;
+  DWORD tsize = 0;
+  do {
+    tcapacity += 4096;
+    TCHAR* _tchars = (TCHAR*) realloc(tchars,sizeof(TCHAR)*tcapacity);
+    if (_tchars == 0) { free(tchars); return 0; }
+    tsize = GetModuleFileName(0,&tchars,tcapacity-1);
+  } while (tsize >= tcapacity-1);
+  
+  tchars[tsize]=0;
+
+  if (sizeof(TCHAR) != 1) {
+    int csize = WideCharToMultiByte(CP_UTF8,WC_COMPOSITECHECK|WC_DEFAULTCHAR,tchars,tsize+1,NULL,0,NULL,NULL);
+    char *chars = (char*) malloc(csize);
+    if (chars != 0) {
+      WideCharToMultiByte(CP_UTF8,WC_COMPOSITECHECK|WC_DEFAULTCHAR,tchars,tsize+1,chars,csize,NULL,NULL);
+    }
+    free(tchars);
+    return chars;
+  } else {
+    char *chars = strdup(tchars);
+    free(tchars);
+    return chars;
+  }
+}
+
+#endif
+
